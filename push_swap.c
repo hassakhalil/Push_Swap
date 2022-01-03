09d05eecafd9576b1f2148a001c74ffb41806026
hassan khalil <hkhalil@student.1337.ma>
2022-01-03 18:52:54 +0100

    starting markup

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 05:20:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2021/12/22 16:52:33 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/01/03 18:51:44 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,11 +64,22 @@ t_list  *indexation(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    t_list  *list = indexation(argc, argv);
-    while (list)
-    {
-        printf("%d\n", list->content);
-        list = list->next;
-    }
+    t_list  *list;
+    char    *greater_than_list;
+    char    *by_index_list;
+
+    //indexation
+    list = indexation(argc, argv);
+    //markup
+    greater_than_list = greater_than_markup(list);
+    by_index_list = by_index_markup(list);
+    if (ft_strlen(greater_than_list) > ft_strlen(by_index_list))
+        print_list(by_index_list);
+    else
+        print_list(greater_than_list);
+    //free everything and return
+    free(list);
+    free(greater_than_list);
+    free(by_index_list);
     return (0);
 }
