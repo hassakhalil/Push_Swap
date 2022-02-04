5a4dcb0412203a356e807df260a39f2b304f01a9
hassan khalil <hkhalil@student.1337.ma>
2022-02-04 18:21:45 +0100

    chunking tasks

--- push_swap.c
+++ push_swap.c
@@ -6,36 +6,38 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 05:20:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/04 17:52:02 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/02/04 18:20:59 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-/*int main(int argc, char *argv[])
-{
-    t_list  *list;
-    char    *greater_than_list;
-    char    *by_index_list;
-
-    //indexation
-    list = indexation(argc, argv);
-    //markup
-    greater_than_list = greater_than_markup(list);
-    by_index_list = by_index_markup(list);
-    if (ft_strlen(greater_than_list) > ft_strlen(by_index_list))
-        print_list(by_index_list);
-    else
-        print_list(greater_than_list);
-    //free everything and return
-    free(list);
-    free(greater_than_list);
-    free(by_index_list);
-    return (0);
-}*/
-
 int main(int argc, char *argv[])
+{
+    t_list  *stack_a;
+	t_list	*stack_b;
+    char    *greater_than_markup;
+    char    *by_index_markup;
+	/*preparing for soliving*/
+    //indexation
+    stack_a = indexation(argc, argv);
+    //markup
+    greater_than_markup = greater_than(stack_a);
+    by_index_markup = by_index(stack_a);
+    
+    /*form stack A to stack B*/
+
+	/*from stack B to stack A*/
+
+	/*align stack A*/
+    
+
+	/*perfome algo with both markups compare and dispay better result*/
+    return (0);
+}
+
+/*int main(int argc, char *argv[])
 {
 	t_list	*stack_a = indexation(argc, argv);
 	while (stack_a)
@@ -43,4 +45,4 @@ int main(int argc, char *argv[])
 		printf("%d\n", stack_a->content);
 		stack_a = stack_a->next;
 	}
-}
+}*/
