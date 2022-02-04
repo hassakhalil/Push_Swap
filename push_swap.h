ee9444b8b9936ba8080033e9ac7d01ab47122b17
hassan khalil <hkhalil@student.1337.ma>
2022-02-04 18:25:51 +0100

    chunking tasks

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 16:15:44 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/02 20:02:17 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/02/04 18:24:46 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@
 #include <stdlib.h>
 #include "./libft/libft.h"
 t_list  *indexation(int argc, char *argv[]);
+char	*greater_than(t_list *stack_a);
+char	*by_index(t_list *stack_a);
 #endif
