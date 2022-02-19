0d1ab9fc9014b8e1e85c92adab5467b7ebd98385
hassan khalil <hkhalil@student.1337.ma>
2022-02-19 19:03:44 +0100

    fixing greater_than function

--- greater_than.c
+++ greater_than.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:28:07 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/18 19:48:03 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/02/19 19:01:53 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int		*greater_than(t_list *stack_a, int argc)
 {
 	t_list	*markup_head;
-	t_list	*head;
+	t_list	*head1;
+	t_list	*head2;
 	t_list	*tmp;
 	int		i;
 	int		j;
 	int		n;
 	int		*markup;
-	int		*tmp_markup;
 
 	markup = malloc(sizeof(int *) * (argc - 1));
 	markup_head = stack_a;
@@ -30,12 +30,18 @@ int		*greater_than(t_list *stack_a, int argc)
 	{
 		i = 0;
 		n = argc - 1;
-		head = markup_head;
+		head1 = markup_head;
+		head2 = markup_head;
 		while (n)
 		{
-			if (head->content < head->next->content)
+			if (head1->content < head2->next->content)
+			{
 				i++;
-			head = head->next;
+				head1 = head1->next;
+				head2 = head2->next;
+			}
+			else
+				head2 = head2->next;
 			n--;
 		}
 		if (j < i)
@@ -46,6 +52,7 @@ int		*greater_than(t_list *stack_a, int argc)
 		markup_head = markup_head->next;
 	}
 	markup_head = tmp;
+	printf("%d\n", tmp->content);
 	n = argc - 1;
 	markup[argc - 1 - ft_lstsize(tmp)] = 1;
 	while (n)
