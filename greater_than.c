a11be1be883d324d4216d7bfd77a617e0a1e977b
hassan khalil <hkhalil@student.1337.ma>
2022-02-24 16:27:39 +0100

    fixing greater_than function

--- greater_than.c
+++ greater_than.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:28:07 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/24 16:10:57 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/02/24 16:27:23 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,20 +73,19 @@ int		*greater_than(t_list *stack_a, int argc)
 		tmp = markup_head;
 	}
 	n = argc - 1;
-	markup[tmp->index] = 1;
+	markup[tmp->next->index] = 6;
 	head2 = tmp;
-	printf("%d\n", tmp->content);
 	while (n)
 	{
 		if (tmp->content < head2->next->content)
 		{
-			markup[head2->index] = 1;
+			markup[head2->next->index] = 1;
 			head2 = head2->next;
 			tmp = head2;
 		}
 		else
 		{
-			markup[head2->index] = 0;
+			markup[head2->next->index] = 0;
 			head2 = head2->next;
 		}
 		n--;
