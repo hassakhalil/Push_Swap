f95082fd8e995faf4e660cd35eaa9c3043d9bc9f
hassan khalil <hkhalil@student.1337.ma>
2022-02-25 18:42:09 +0100

    greater_than function fixed

--- greater_than.c
+++ greater_than.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:28:07 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/25 18:31:58 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/02/25 18:40:49 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@ t_list	*choosing_markup_head(t_list *stack_a, int argc)
 	t_list	*tmp1;
 	t_list	*tmp2;
 	t_list	*tmp;
+	int		flag;
 	int		i;
 	int		j;
 	int		n;
 
 	tmp = stack_a;
 	j = 0;
-	while (tmp->next != stack_a)
+	flag = 0;
+	while (tmp != stack_a || !flag)
 	{
+		flag = 1;
 		i = 0;
 		n = argc - 1;
 		tmp1 = tmp;
@@ -48,27 +51,6 @@ t_list	*choosing_markup_head(t_list *stack_a, int argc)
 			markup_head = tmp;
 		}
 		tmp = tmp->next;
-	}
-	i = 0;
-	n = argc - 1;
-	tmp1 = tmp;
-	tmp2 = tmp;
-	while (n)
-	{
-		if (tmp1->content < tmp2->next->content)
-		{
-			i++;
-			tmp2 = tmp2->next;
-			tmp1 = tmp2;
-		}
-		else
-			tmp2 = tmp2->next;
-		n--;
-	}
-	if (j < i)
-	{
-		j = i;
-		markup_head = tmp;
 	}
 	return (markup_head);
 }
