0ba96b4c7fb4c1e3ee2b0e0d2fe1ea3d6d1d1353
hassan khalil <hkhalil@student.1337.ma>
2022-03-01 14:36:26 +0100

    fixing push function

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 15:20:36 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/02/28 20:47:06 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/03/01 14:35:44 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 void	push(t_list **stack_x, t_list **stack_y)
 {
-	t_list	*tmp;
 	t_list	*head;
-	t_list	*node;
+	t_list	*curr;
+	t_list	*prev;
+	t_list	*tmp;
 
-	ft_lstadd_front(stack_y, ft_lstnew(*stack_x)->content));
-	tmp = *stack_x;
 	head = *stack_x;
-	node = *stack_x;
-	ft_lstlast(tmp)->next = head->next;
-	stack_x = &(head->next);
-	node->next = 0;
-	free(node);
+	ft_lstadd_front(stack_y, ft_lstnew((*stack_x)->content));
+	curr = head;
+	prev = curr;
+	head = head->next;
+	ft_lstlast(prev)->next = head;
+	tmp  = head;
+	stack_x = &tmp;
+	free(curr);
 }
