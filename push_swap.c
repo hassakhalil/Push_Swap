9b6925b3c98a574e59ebcf89cf3347eb8e07bc97
hassan khalil <hkhalil@student.1337.ma>
2022-03-06 06:18:49 +0100

    fixing push function

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 05:20:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/03/06 06:14:33 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/03/06 06:18:26 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,6 @@ int main(int argc, char *argv[])
     //markup
     greater_than_markup = greater_than(stack_a, argc);
 	by_index_markup = by_index(stack_a, argc);
-	//testing
-	//swap(stack_a);
-	//rotate(&stack_a, 0);
-	push(&stack_a, &stack_b);
-	push(&stack_a, &stack_b);
-	push(&stack_a, &stack_b);
-	tmp = stack_a;
-	while (tmp->next != stack_a)
-	{
-		printf("----%d\n", tmp->content);
-		tmp = tmp->next;
-	}
-	printf("----%d\n", tmp->content);
-
-	tmp = stack_b;
-	while (tmp->next != stack_b)
-	{
-		printf("++++%d\n", tmp->content);
-		tmp = tmp->next;
-	}
-	printf("++++%d\n", tmp->content);
     /*form stack A to stack B*/
 
 	/*from stack B to stack A*/
