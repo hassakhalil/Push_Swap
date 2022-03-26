5e27777fed63d8212871cfa7613c56ef2955abd4
hassan khalil <hkhalil@student.1337.ma>
2022-03-26 15:17:48 +0100

    coding from stack_b to stack_a function

---
+++ b_to_a.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   b_to_a.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 15:07:40 by hkhalil           #+#    #+#             */
+/*   Updated: 2022/03/26 15:17:03 by hkhalil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	b_to_a(t_list **stack_a, t_list **stack_b, int markup_mode)
+{
+
+	while (stack_b is not empty)
+	{
+		//choose element for moving
+		//move a and b to prepare for push
+		push(stack_b, stack_a, markup_mode);
+	}
+}
--- check_swap.c
+++ check_swap.c
@@ -6,42 +6,41 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 15:51:36 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/03/23 20:27:57 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/03/23 21:24:28 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	check_swap(t_list **stack, int markup_mode)
+int	check_swap_h(t_list	**stack, int markup_mode)
 {
-	int		j;
-	int		k;
 	t_list	*head;
+	int		i;
 
-	j = 0;
+	i = 0;
 	head = *stack;
 	while ((*stack)->next != head)
 	{
 		if ((*stack)->markup == 1)
-			j++;
+			i++;
 		(*stack) = (*stack)->next;
 	}
+	printf("hellooooo\n");
 	if ((*stack)->markup == 1)
-		j++;
+		i++;
 	(*stack) = (*stack)->next;
 	swap(stack);
 	markup(stack, markup_mode);
-	k = 0;
-	while ((*stack)->next != head)
-	{
-		if ((*stack)->markup == 1)
-			k++;
-		(*stack) = (*stack)->next;
-	}
-	if ((*stack)->markup == 1)
-		k++;
-	(*stack) = (*stack)->next;
-	swap(stack);
+	return (i);
+}
+
+int	check_swap(t_list **stack, int markup_mode)
+{
+	int		j;
+	int		k;
+
+	j = check_swap_h(stack, markup_mode);
+	k = check_swap_h(stack, markup_mode);
 	if (k > j)
 		return (1);
 	return (0);
