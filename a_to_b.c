da7afd437ad0366a043b349077529c20bbc04c49
hassan khalil <hkhalil@student.1337.ma>
2022-04-02 02:11:30 +0000

    fixing bugs

--- a_to_b.c
+++ a_to_b.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 14:16:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/03/23 20:24:48 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/04/02 02:10:19 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,41 @@ int	check_zero(t_list *stack)
 		tmp = tmp->next;
 	}
 	if (tmp->markup == 0)
+		return (1);
+	return (0);
+}
+int	check_swap(t_list **stack, int markup_mode)
+{
+	int		j;
+	int		k;
+	t_list	*head;
+
+	j = 0;
+	k = 0;
+	head = *stack;
+	while ((*stack)->next != head)
+	{
+		if ((*stack)->markup == 1)
+			j++;
+		(*stack) = (*stack)->next;
+	}
+	if ((*stack)->markup == 1)
+		j++;
+	(*stack) = (*stack)->next;
+	swap(stack);
+	markup(stack, markup_mode);
+	while ((*stack)->next != head)
+	{
+		if ((*stack)->markup == 1)
+			k++;
+		(*stack) = (*stack)->next;
+	}
+	if ((*stack)->markup == 1)
+		k++;
+	swap(stack);
+	markup(stack, markup_mode);
+	(*stack) = (*stack)->next;
+	if (k > j)
 		return (1);
 	return (0);
 }
