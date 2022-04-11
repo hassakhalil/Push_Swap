e0b08cca0c8750e11741be44d5788bdda9710d86
hassan khalil <hkhalil@student.1337.ma>
2022-04-11 04:00:10 +0000

    almost done

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 05:20:26 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/04/10 22:33:55 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/04/11 03:59:37 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int argc, char *argv[])
     if (argc > 2 && !error)
 	{
 		stack_b = NULL;
-		markup_mode = 0;
+		markup_mode = 1;
 		stack_a = indexation(argc, argv);
 		a_to_b(&stack_a, &stack_b, markup_mode);
 		if (stack_b)
