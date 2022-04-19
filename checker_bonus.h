07db64cb96468fb9e45181701b6c7282499a7ba6
hassan khalil <hkhalil@student.1337.ma>
2022-04-19 01:45:33 +0000

    norm

--- checker_bonus.h
+++ checker_bonus.h
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 01:43:57 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/04/13 22:02:10 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/04/19 01:45:05 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 # include "push_swap.h"
 # include "get_next_line.h"
 
-void    checker(t_list *stack_a);
-int	    ft_strncmp(const char *s1, const char *s2, size_t n);
-char    *ft_strjoin(char const *s1, char const *s2);
-void    *ft_memcpy(void *dest, const void *src, size_t n);
-int     check_words(char **instructions);
+void	checker(t_list *stack_a);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		check_words(char **instructions);
 
 #endif
