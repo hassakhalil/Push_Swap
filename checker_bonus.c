0602b400d7a772ef2bb8997872fb94bf0cf4ddd3
hassan khalil <hkhalil@student.1337.ma>
2022-04-23 03:23:51 +0000

    norm

--- checker_bonus.c
+++ checker_bonus.c
@@ -25,34 +25,62 @@ int	execution(char **instruction, t_list **stack_a, t_list **stack_b)
 		return (-1);
 	while (instruction[i])
 	{
-		if (!ft_strncmp("sa", instruction[i], ft_strlen(instruction[i])))
+		if (!ft_strncmp("sa", instruction[i], 3))
 			swap(stack_a);
-		else if (j && !ft_strncmp("sb", instruction[i], ft_strlen(instruction[i])))
-			swap(stack_b);
-		else if ((j + 1 < n) &&!ft_strncmp("pb", instruction[i], ft_strlen(instruction[i])))
+		else if (!ft_strncmp("sb", instruction[i], 3))
 		{
+			if (!j)
+				return (1);
+			swap(stack_b);
+		}
+		else if (!ft_strncmp("pb", instruction[i], 3))
+		{
+			if (!(j + 1 < n))
+				return (1);
 			push(stack_a, stack_b);
 			j++;
 		}
-		else if (!ft_strncmp("ra", instruction[i], ft_strlen(instruction[i])))
+		else if (!ft_strncmp("ra", instruction[i], 3))
 			rotate(stack_a, 0);
-		else if (!ft_strncmp("rra", instruction[i], ft_strlen(instruction[i])))
+		else if (!ft_strncmp("rra", instruction[i], 4))
 			rotate(stack_a, 1);
-		else if (j && !ft_strncmp("rb", instruction[i], ft_strlen(instruction[i])))
-			rotate(stack_b, 0);
-		else if (j && !ft_strncmp("rrb", instruction[i], ft_strlen(instruction[i])))
-			rotate(stack_b, 1);
-		else if (j && !ft_strncmp("pa", instruction[i], ft_strlen(instruction[i])))
+		else if (!ft_strncmp("rb", instruction[i], 3))
 		{
+			if (!j)
+				return (1);
+			rotate(stack_b, 0);
+		}
+		else if (!ft_strncmp("rrb", instruction[i], 4))
+		{
+			if (!j)
+				return (1);
+			rotate(stack_b, 1);
+		}
+		else if (!ft_strncmp("pa", instruction[i], 3))
+		{
+			if (!j)
+				return (1);
 			push(stack_b, stack_a);
 			j--;
 		}
-		else if (j && !ft_strncmp("ss", instruction[i], ft_strlen(instruction[i])))
+		else if (!ft_strncmp("ss", instruction[i], 3))
+		{
+			if (!j)
+				return (1);
 			ss(stack_a, stack_b);
-		else if (j && !ft_strncmp("rr", instruction[i], ft_strlen(instruction[i])))
+		}
+		else if (!ft_strncmp("rr", instruction[i], 3))
+		{
+			if (!j)
+				return (1);
 			rr(stack_a, stack_b, 0);
-		else if (j && !ft_strncmp("rrr", instruction[i], ft_strlen(instruction[i])))
+		}
+		else if (!ft_strncmp("rrr", instruction[i], 4))
+		{
+			if (!j)
+				return (1);
 			rr(stack_a, stack_b, 1);
+		}
 		i++;
 	}
 	if (j)
@@ -68,7 +96,10 @@ int	verification(t_list *stack_a)
 	while (head->next != stack_a)
 	{
 		if (head->content > head->next->content)
+		{
+			printf("%d\n", head->content);
 			return (1);
+		}
 		head = head->next;
 	}
 	return (0);
@@ -110,6 +141,7 @@ void	checker(t_list *stack_a)
 	}
 	if (verification(stack_a))
 	{
+		printf("hello|||||||||||||| world!\n");
 		write(1, "KO\n", 3);
 		return ;
 	}
